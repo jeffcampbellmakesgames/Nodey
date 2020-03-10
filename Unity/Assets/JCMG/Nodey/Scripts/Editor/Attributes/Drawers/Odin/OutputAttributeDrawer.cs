ee8d7d368a48026463d31cc5f5665f356473234a
Jeff Campbell <jeff@zapdot.com>
2020-03-10 09:37:43 +0100

    Fixed compile issues with Odin Integration
    * Fixed compile issues with Odin Integration resulting from API and code structure changes not automatically applied to #if #endif blocked out code.

--- Unity/Assets/JCMG/Nodey/Scripts/Editor/Attributes/Drawers/Odin/OutputAttributeDrawer.cs
+++ Unity/Assets/JCMG/Nodey/Scripts/Editor/Attributes/Drawers/Odin/OutputAttributeDrawer.cs
@@ -3,49 +3,66 @@ using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
 using Sirenix.Utilities.Editor;
 using UnityEngine;
-using XNode;
 
-namespace XNodeEditor {
-	public class OutputAttributeDrawer : OdinAttributeDrawer<XNode.Node.OutputAttribute> {
-		protected override bool CanDrawAttributeProperty(InspectorProperty property) {
-			Node node
- = property.Tree.WeakTargets[0] as Node;
+namespace JCMG.Nodey.Editor
+{
+	public class OutputAttributeDrawer : OdinAttributeDrawer<OutputAttribute>
+	{
+		protected override bool CanDrawAttributeProperty(InspectorProperty property)
+		{
+			var node = property.Tree.WeakTargets[0] as Node;
 			return node != null;
 		}
 
-		protected override void DrawPropertyLayout(GUIContent label) {
-			Node node
- = Property.Tree.WeakTargets[0] as Node;
-			NodePort port
- = node.GetOutputPort(Property.Name);
+		protected override void DrawPropertyLayout(GUIContent label)
+		{
+			var node = Property.Tree.WeakTargets[0] as Node;
+			var port = node.GetOutputPort(Property.Name);
 
-			if (!NodeEditor.inNodeEditor) {
-				if (Attribute.backingValue == XNode.Node.ShowBackingValue.Always || Attribute.backingValue == XNode.Node.ShowBackingValue.Unconnected && !port.IsConnected)
+			if (!NodeEditor.inNodeEditor)
+			{
+				if (Attribute.backingValue == ShowBackingValue.Always ||
+				    Attribute.backingValue == ShowBackingValue.Unconnected && !port.IsConnected)
+				{
 					CallNextDrawer(label);
+				}
+
 				return;
 			}
 
-			if (Property.Tree.WeakTargets.Count > 1) {
+			if (Property.Tree.WeakTargets.Count > 1)
+			{
 				SirenixEditorGUI.WarningMessageBox("Cannot draw ports with multiple nodes selected");
 				return;
 			}
 
-			if (port != null) {
+			if (port != null)
+			{
 				var portPropoerty
- = Property.Tree.GetUnityPropertyForPath(Property.UnityPropertyPath);
-				if (portPropoerty == null) {
+					= Property.Tree.GetUnityPropertyForPath(Property.UnityPropertyPath);
+				if (portPropoerty == null)
+				{
 					SirenixEditorGUI.ErrorMessageBox("Port property missing at: " + Property.UnityPropertyPath);
-					return;
-				} else {
+				}
+				else
+				{
 					var labelWidth
- = Property.GetAttribute<LabelWidthAttribute>();
+						= Property.GetAttribute<LabelWidthAttribute>();
 					if (labelWidth != null)
+					{
 						GUIHelper.PushLabelWidth(labelWidth.Width);
+					}
 
-					NodeEditorGUILayout.PropertyField(portPropoerty, label == null ? GUIContent.none : label, true, GUILayout.MinWidth(30));
+					NodeEditorGUILayout.PropertyField(
+						portPropoerty,
+						label == null ? GUIContent.none : label,
+						true,
+						GUILayout.MinWidth(30));
 
 					if (labelWidth != null)
+					{
 						GUIHelper.PopLabelWidth();
+					}
 				}
 			}
 		}
--- Unity/Assets/JCMG/Nodey/Scripts/Editor/Inspectors/Graphs/GlobalGraphEditor.cs
+++ Unity/Assets/JCMG/Nodey/Scripts/Editor/Inspectors/Graphs/GlobalGraphEditor.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 #if ODIN_INSPECTOR
 using Sirenix.OdinInspector.Editor;
-using Sirenix.Utilities;
-using Sirenix.Utilities.Editor;
 #endif
 
 namespace JCMG.Nodey.Editor
@@ -12,14 +10,18 @@ namespace JCMG.Nodey.Editor
 	/// <summary> Override graph inspector to show an 'Open Graph' button at the top </summary>
 	[CustomEditor(typeof(NodeGraph), true)]
 	#if ODIN_INSPECTOR
-    public class GlobalGraphEditor : OdinEditor {
-        public override void OnInspectorGUI() {
-            if (GUILayout.Button("Edit graph", GUILayout.Height(40))) {
-                NodeEditorWindow.Open(serializedObject.targetObject as XNode.NodeGraph);
-            }
-            base.OnInspectorGUI();
-        }
-    }
+	public class GlobalGraphEditor : OdinEditor
+	{
+		public override void OnInspectorGUI()
+		{
+			if (GUILayout.Button("Edit graph", GUILayout.Height(40)))
+			{
+				NodeEditorWindow.Open(serializedObject.targetObject as NodeGraph);
+			}
+
+			base.OnInspectorGUI();
+		}
+	}
 	#else
 	public class GlobalGraphEditor : UnityEditor.Editor
 	{
